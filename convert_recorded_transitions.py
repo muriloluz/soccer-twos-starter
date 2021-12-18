72a41d91fc4ae4c0dbbf7ccc90f20bc9833b7235
Bryan Lincoln <bryanufg@gmail.com>
2021-12-17 21:34:41 -0300

    feat: add transition converter

---
+++ convert_recorded_transitions.py
@@ -0,0 +1,57 @@
+import os
+
+import json_tricks
+from ray.rllib.models.preprocessors import get_preprocessor
+from ray.rllib.evaluation.sample_batch_builder import SampleBatchBuilder
+from ray.rllib.offline.json_writer import JsonWriter
+from tqdm import tqdm
+
+from transition_recorder_wrapper import TransitionRecorderWrapper
+
+
+RAW_DS_PATH = "./data/raw"
+PROCESSED_DS_PATH = "./data/processed"
+
+
+if __name__ == "__main__":
+    batch_builder = SampleBatchBuilder()
+    writer = JsonWriter(PROCESSED_DS_PATH)
+
+    # load each raw episode group file
+    for file in tqdm(os.listdir(RAW_DS_PATH)):
+        if file.endswith(".jsonl"):
+            with open(os.path.join(RAW_DS_PATH, file), "r") as f:
+                # each line on this file is an episode
+                episode_s = f.readline()
+                while episode_s:
+                    episode = json_tricks.loads(episode_s)
+                    if type(episode) is str:
+                        episode = TransitionRecorderWrapper.uncompress_data(episode)
+
+                    for t, obs, action, new_obs, reward, done in zip(
+                        range(len(episode["obs"])),
+                        episode["obs"],
+                        episode["action"],
+                        episode["new_obs"],
+                        episode["reward"],
+                        episode["done"],
+                    ):
+                        # any MDP-related reprocessing would happen here
+
+                        # add to batch
+                        batch_builder.add_values(
+                            t=t,
+                            eps_id=episode["episode_id"],
+                            agent_index=episode["agent_id"],
+                            obs=obs,
+                            actions=action,
+                            action_prob=1.0,  # put the true action probability here
+                            action_logp=0.0,
+                            rewards=reward,
+                            dones=done,
+                            new_obs=new_obs,
+                        )
+
+                    # write file & load new episode
+                    writer.write(batch_builder.build_and_reset())
+                    episode_s = f.readline()
